31958a0e6c0ee855954ce13f97253f7b0cfe68ce
Karl Godin <godinkarl@gmail.com>
2017-02-02 20:14:12 -0500

    Character Movement Demo with Keyboard

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	public GameObject character;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddForce (new Vector3 (0, 0, 1));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rb.AddForce (new Vector3 (0, 0, -1));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb.AddForce (new Vector3 (1, 0, 0));
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rb.AddForce (new Vector3 (-1, 0, 0));
+		}
+	}
+}

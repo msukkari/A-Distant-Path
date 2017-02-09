e8b7863e11d271787b4a6e480420b24cd6561fe3
Karl Godin <godinkarl@gmail.com>
2017-02-09 18:12:00 -0500

    Added linear interpolation for movement and fixed a bug which caused the camera to spin out of control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,13 +13,24 @@ public class CameraController : MonoBehaviour {
 	public float minZoom = 1.0f;
 
 	public GameObject character;
-	float dragFactor = 0.1f;
+	private float height;
+	public GameObject pivot;
+	float dragFactor = 3.0f;
 
-	public float rotateSpeed;
+	private float rotateSpeed = 125.0f;
+	private float rotateThreshold = 0.2f;
+
+	Vector3 turnDir = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		cam = GetComponentInChildren<Camera> ();
+		height = character.GetComponent<CapsuleCollider> ().height;
+
+		cam = pivot.GetComponentInChildren<Camera> ();
+		x = pivot.transform.rotation.y * 360/Mathf.PI;
+		Debug.Log ("X: " + x);
+		y = pivot.transform.rotation.x * 360/Mathf.PI;
+		Debug.Log ("Y: " + y);
 	}
 
 	private float x = 0;
@@ -27,26 +38,35 @@ public class CameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 turnDir = Vector3.zero;
+		//Camera Displacement
+		Vector3 distance = character.transform.position - transform.position;
+		if (distance.sqrMagnitude > 0) {
+			if (distance.sqrMagnitude < 0.0001) {
+				transform.position = character.transform.position;
+			} else {
+				float x = Mathf.Lerp (transform.position.x, character.transform.position.x, Time.deltaTime * dragFactor);
+				float z = Mathf.Lerp (transform.position.z, character.transform.position.z, Time.deltaTime * dragFactor);
+
+				transform.position = new Vector3 (x, character.transform.position.y, z);
+			}
+
+		}
+
+		//Camera Rotation
 		turnDir.x = Input.GetAxis ("RightJoystickHorizontal");
 		turnDir.y = Input.GetAxis ("RightJoystickVertical");
 
-		if (turnDir.sqrMagnitude >= 0.1) {
-			x += turnDir.x;
-			if (turnDir.y >= 0.4 || turnDir.y <= -0.4) {
-				y += turnDir.y;
-			}
-			y = Mathf.Clamp (y, 10, 70);
-			transform.rotation = Quaternion.Euler (new Vector3 (y, x, 0));
-		}
-		Vector3 distance = character.transform.position - transform.position;
-		if (distance.sqrMagnitude > 0) {
-			if (distance.sqrMagnitude < 0.01) {
-				transform.position = character.transform.position;
-			} else {
-				transform.Translate(distance * dragFactor);
+		if (turnDir.sqrMagnitude >= rotateThreshold) {
+			if (turnDir.x >= rotateThreshold || turnDir.x <= -rotateThreshold) {
+				x = Mathf.LerpAngle (x, turnDir.x * rotateSpeed + x, Time.deltaTime);
 			}
 
+			if (turnDir.y >= rotateThreshold || turnDir.y <= -rotateThreshold) {
+				y = Mathf.Lerp (y, turnDir.y * rotateSpeed + y, Time.deltaTime);
+			}
+			y = Mathf.Clamp (y, 10, 70);
+			pivot.transform.rotation = Quaternion.Euler (new Vector3 (y, x, 0));
 		}
+
 	}
 }

12fa181b704a223b02248171d7570ae9ac524339
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2017-02-24 09:49:43 -0500

    manually added fire system because merge wouldn't work

---
+++ Assets/Scripts/Elements/Fire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : Element {
+
+	public int IGNITE_THRESHOLD;
+	public List<Tile> nList;
+	public bool fireSystemOn = false;
+
+	// Use this for initialization
+	void Start () {
+		elementType = ElementType.Fire;
+		nList = transform.parent.GetComponent<Tile>().neighbors;
+		IGNITE_THRESHOLD = 1000;
+
+		if(nList == null){
+			Debug.Log("Error getting nList in Fire Element");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fireSystemOn){
+			stepFireSystem();
+		}
+	}
+
+
+
+
+	private void stepFireSystem(){
+		int roll = Random.Range(0, IGNITE_THRESHOLD) + 1;
+
+		if(roll == IGNITE_THRESHOLD){
+			Debug.Log("IGNITE THRESHOLD HIT!");
+			Tile hitTile = nList[Random.Range(0, nList.Count)];
+
+			if(hitTile.element == null){
+				hitTile.GainElement(ElementType.Fire);
+			}
+		}
+	}
+}

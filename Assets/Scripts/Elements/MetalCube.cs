94313829ce1fc3fab59bbb5adb581d78ace83b86
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2017-02-23 23:32:49 -0500

    updated corrosion

---
+++ Assets/Scripts/Elements/MetalCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalCube : Element {
+
+	public int tileID;
+
+	void Start () {
+		elementType = ElementType.MetalCube;
+		this.tileID = transform.parent.GetComponent<Tile>().getTileID();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

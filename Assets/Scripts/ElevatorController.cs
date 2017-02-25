988a8d457974b17dd2b0bff4db001ecfa793db20
Milan Singh <milanprsingh@gmail.com>
2017-02-24 20:27:47 -0500

    Updated Past, Present added Elevator
    Added Elevator Prefab:
    When player goes enters trigger (Invis cube above the    tile), the tile
    rises to a set height defined in public vector on script
    ElevatorController
    
    Updated Past and Present:
    Both Past and Present now reflect what the final level should be like
    for their respective timelines. (Trees need to be added to present as
    soon as sucking up from Burning Trees is added)

---
+++ Assets/Scripts/ElevatorController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorController : MonoBehaviour {
+	public float speed = 1;
+	public Vector3 startPos;
+	public Vector3 endPos;
+	public GameObject parent;
+
+	private bool isTriggered = false;
+	// Use this for initialization
+	void Start () {
+		startPos = parent.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTriggered & parent.transform.position.y < endPos.y) {
+
+			Debug.Log ("Hit");
+			parent.transform.position += Vector3.up * speed * 0.01f;
+		} else if ((!isTriggered) & parent.transform.position != startPos){
+			parent.transform.position += Vector3.down * speed * 0.01f;
+		}
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.tag == "Player") {
+			isTriggered = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			isTriggered = false;
+		}
+	}
+
+
+	
+}

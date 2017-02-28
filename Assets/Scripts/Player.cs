dfe9c3ce136fcae3aa10933ac01fe083bbb890d9
Nehirakdag <nehirakdag34@hotmail.com>
2017-02-28 18:30:38 -0500

    Revert "Revert "fixed minor network bugs""
    This reverts commit 7146b2d9d4a21bbf463659ba3dbe902c2e828bf0.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -54,7 +54,7 @@ public class Player : MonoBehaviour{
 		}
 
 		// Suck tile only if space is pressed and the player is not moving
-		if ((Input.GetKey (KeyCode.Space) || Input.GetButton("AButton")) && NotMoving()) {
+		if ((Input.GetKey (KeyCode.Space) || Input.GetButton("XButton")) && NotMoving()) {
 			chargingWeapon = true;
 		}
 

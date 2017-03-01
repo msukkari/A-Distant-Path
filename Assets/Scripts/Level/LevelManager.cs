c402c1c669fb0c8d153dc51ad67aaf8d7fa56546
Brendan Kellam <bshizzle1234@gmail.com>
2017-02-28 22:15:50 -0500

    Added Nehir's GetClosestTileOfType function to LevelManager

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -168,6 +168,27 @@ public class LevelManager : MonoBehaviour {
 
 		tile.SetNavigatable (false);
 	}
+
+	public Tile GetClosestTileOfType(ElementType elemType, Vector3 position) {
+		bool first = true;
+		Tile closestTile = null;
+
+		for (int i = 0; i < tileList.Count - 1; i++) {
+			if(tileList[i].element != null && tileList[i].element.elementType == elemType) {
+				if(first) {
+					closestTile = tileList[i];
+					first = false;
+				}
+				else{
+					if(Mathf.Abs(Vector3.Distance(position, tileList[i].transform.position)) < 
+						Mathf.Abs(Vector3.Distance(position, closestTile.transform.position))) {
+						closestTile = tileList[i];
+					}
+				}
+			}
+		}
+		return closestTile;
+	}
 	
 	// Update is called once per frame
 	void Update () { }

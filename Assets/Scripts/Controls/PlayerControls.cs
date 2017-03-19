09828965ebdb589c5b7c815394a3fe045b5d828b
Karl Godin <godinkarl@gmail.com>
2017-03-19 14:11:55 -0400

    Added simple UI to represent the amount of fire and water the player has

--- Assets/Scripts/Controls/PlayerControls.cs
+++ Assets/Scripts/Controls/PlayerControls.cs
@@ -154,9 +154,6 @@ public class PlayerControls : MonoBehaviour {
 
         prevSelectedTile = curTile;
 
-
-        Debug.Log(isShooting);
-
         if (Input.GetAxisRaw("LeftTrigger") >= 0.9 && !isShooting ) {
         	isShooting = true;
             if (mode == TriggerType.arcThrowing) {

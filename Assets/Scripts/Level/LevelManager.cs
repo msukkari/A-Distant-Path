87cd393e7eb3ebdacf4f7ca9b8924d390b37b572
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2017-03-21 06:39:06 -0400

    fixed spawn

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -99,10 +99,10 @@ public class LevelManager : MonoBehaviour {
 
 		GameObject player;
 		if(TimeState == TimeStates.Present || TimeState == TimeStates.Offline){
-			player = Instantiate(futurePlayerPrefab, new Vector3(5f, 2.5f, 2.5f), Quaternion.identity) as GameObject;
+			player = Instantiate(futurePlayerPrefab, new Vector3(5f, 2.5f, 6f), Quaternion.identity) as GameObject;
 		}
 		else{
-			player = Instantiate(pastPlayerPrefab, new Vector3(5f, 2.5f, 2.5f), Quaternion.identity) as GameObject;			
+			player = Instantiate(pastPlayerPrefab, new Vector3(5f, 2.5f, 6f), Quaternion.identity) as GameObject;			
 		}
 
 		this.player = player.GetComponent<Player>();

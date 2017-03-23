51cc9c7fae13d5b781ca71d34db3731bdea07b33
Milan Singh <milanprsingh@gmail.com>
2017-03-23 13:36:59 -0400

    Credits

---
+++ Assets/textController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textController : MonoBehaviour {
+    public RectTransform t;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RectTransform t = this.GetComponentInParent<RectTransform>();
+        t.anchoredPosition += Vector2.up;
+	}
+}

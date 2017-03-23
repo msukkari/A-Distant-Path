ec8dc4e9ae96721d82aa1b3424869a14fad0c63c
msukkarieh <michael.sukkarieh@mail.mcgill.ca>
2017-03-23 13:20:37 -0400

    added mute audio trigger

---
+++ Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteAudio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void OnTriggerEnter(Collider other){
+		Debug.Log("TRIGGER ON CHECKPOINT");
+
+		Player player = other.gameObject.GetComponent<Player>();
+
+		if(player != null){
+			AudioManager.instance.primary.Stop();		
+		}
+
+	}
+}

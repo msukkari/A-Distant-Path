ceb42d7dbc9569fb1112613c676a92c53eb70e1c
Brendan Kellam <bshizzle1234@gmail.com>
2017-03-22 22:24:20 -0400

    Added tree sounds

--- Assets/Scripts/Elements/BigTree.cs
+++ Assets/Scripts/Elements/BigTree.cs
@@ -6,10 +6,12 @@ public class BigTree : Element {
 
 	public int tileID;
 
+	public AudioSource audio;
+
 	void Start () {
 		elementType = ElementType.BigTree;
 		this.tileID = transform.parent.GetComponent<Tile>().getTileID();
-
+		this.audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame

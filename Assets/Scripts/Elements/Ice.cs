36de05118ede70ff3c511a0c6bec50963ec7b9b8
Milan Singh <milanprsingh@gmail.com>
2017-03-22 21:09:18 -0400

    Fixed Ice Null Error

--- Assets/Scripts/Elements/Ice.cs
+++ Assets/Scripts/Elements/Ice.cs
@@ -2,81 +2,115 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ice : Element {
+public class Ice : Element
+{
 
-	public int tileID;
+    public int tileID;
+    public GameObject player;
 
-	public GameObject iceCubePrefab;
+    public GameObject iceCubePrefab;
     //public GameObject icePrefab;
     //public List<GameObject> iceCubes = new List<GameObject> ();
 
-	void Start () {
-		elementType = ElementType.Ice;
-		this.tileID = transform.parent.GetComponent<Tile>().getTileID();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        elementType = ElementType.Ice;
+        this.tileID = transform.parent.GetComponent<Tile>().getTileID();
+        this.player = GameObject.FindGameObjectWithTag("Player");
+    }
 
-	public override bool WaterInteract(EventTransferManager ETManager) {
-		/*if (!this.GetComponent<MeshRenderer> ().enabled) {
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override bool WaterInteract(EventTransferManager ETManager)
+    {
+        /*if (!this.GetComponent<MeshRenderer> ().enabled) {
 			this.GetComponent<MeshRenderer> ().enabled = true;
 			return true;
 		} else {
 			return false;
 		}*/
-		CreateIceCube ();
-		return true;
-	}
+        CreateIceCube();
+        return true;
+    }
 
-	public override bool FireInteract(EventTransferManager ETManager) {
-		//this.GetComponentInParent<Tile> ().GainElement (ElementType.Fire);
-		DestroyIceCube();
-		return true;
-	}
+    public override bool FireInteract(EventTransferManager ETManager)
+    {
+        //this.GetComponentInParent<Tile> ().GainElement (ElementType.Fire);
+        DestroyIceCube();
+        return true;
+    }
 
-	private void CreateIceCube() {
-		Level level = GameObject.FindGameObjectWithTag ("Level").GetComponent<Level> ();
+    private void CreateIceCube()
+    {
+        Level level = GameObject.FindGameObjectWithTag("Level").GetComponent<Level>();
 
-		if (level != null) {
-			Tile topTile = this.GetComponentInParent<Tile> ().GetTopTile ();
-			GameObject newIceCubeTile = Instantiate (iceCubePrefab, topTile.transform.position + new Vector3(0.0f, topTile.tileScale.y ,0.0f), Quaternion.identity, GameObject.FindGameObjectWithTag("Level").transform) as GameObject;
-			newIceCubeTile.tag = "Tile";
-			newIceCubeTile.GetComponent<Tile> ().isGroundTile = false;
+        if (level != null)
+        {
+            Tile topTile = this.GetComponentInParent<Tile>().GetTopTile();
+            GameObject newIceCubeTile = Instantiate(iceCubePrefab, topTile.transform.position + new Vector3(0.0f, topTile.tileScale.y, 0.0f), Quaternion.identity, GameObject.FindGameObjectWithTag("Level").transform) as GameObject;
+            newIceCubeTile.tag = "Tile";
+            newIceCubeTile.GetComponent<Tile>().isGroundTile = false;
             newIceCubeTile.GetComponent<Tile>().EventManager = topTile.EventManager;
 
-            GameObject newIceElement = Instantiate (this.gameObject, newIceCubeTile.transform);
-			// Should probably assign a unique id to the created tile as well!
-			LevelManager.instance.AddTileToList (newIceCubeTile.GetComponent<Tile> ());
-		}
-	}
+            GameObject newIceElement = Instantiate(this.gameObject, newIceCubeTile.transform);
+            // Should probably assign a unique id to the created tile as well!
+            LevelManager.instance.AddTileToList(newIceCubeTile.GetComponent<Tile>());
+        }
+    }
 
-	private void DestroyIceCube() {
-		Tile topTile = this.GetComponentInParent<Tile> ().GetTopTile ();
-		//if (topTile.isGroundTile) {
-		//	topTile.SetNavigatable (true);
+    private void DestroyIceCube()
+    {
+        Tile topTile = this.GetComponentInParent<Tile>().GetTopTile();
+        //if (topTile.isGroundTile) {
+        //	topTile.SetNavigatable (true);
         //    Destroy(topTile.gameObject);
-		//} else {
-		//	topTile.gameObject.SetActive (false);
-		//}
+        //} else {
+        //	topTile.gameObject.SetActive (false);
+        //}
         RaycastHit hit;
         Ray ray = new Ray(topTile.transform.position, Vector3.down);
-        if (Physics.Raycast(ray, out hit)){
-            Tile hitTile = hit.collider.GetComponentInParent<Tile>();       
-                if (hitTile.element != null ){
-                    if (hitTile.element.elementType != ElementType.Ice){
-                        hitTile.element.WaterInteract(hitTile.EventManager);
+        bool isTile = false;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Component[] Components = hit.collider.GetComponentsInParent<Component>();
+            for (int i = 0; i < Components.GetLength(0); i++)
+            {
+                Component current = Components[i];
+                if (current.tag == "Tile")
+                {
+                    Tile hitTile = hit.collider.GetComponentInParent<Tile>();
+                    isTile = true;
+                    if (hitTile.element != null)
+                    {
+                        if (hitTile.element.elementType != ElementType.Ice)
+                        {
+                            hitTile.element.WaterInteract(hitTile.EventManager);
+                        }
                     }
-                } else {
-                    hitTile.GainElement(ElementType.Water);
+                    else
+                    {
+                        hitTile.GainElement(ElementType.Water);
+                    }
+                    i = Components.GetLength(0);
                 }
+            }
         }
-        topTile.gameObject.SetActive(false);
-	}
+        if (isTile)
+        {
+            topTile.gameObject.SetActive(false);
+        }
+        else
+        {
+            Player playerScript = player.GetComponent<Player>();
+            playerScript.GainElement(ElementType.Fire, 1);
+        }
+    }
 
-	/*private void CreateOrDestroyIceBlock(bool create) {
+    /*private void CreateOrDestroyIceBlock(bool create) {
 		Element[] stacksOfIce = this.GetComponentInParent<Tile> ().GetComponentsInChildren<Ice> ();
 
 		if (create) {

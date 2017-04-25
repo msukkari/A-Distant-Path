bf74b23afc6a390c876b06161f8ce492b7816217
Brendan Kellam <bshizzle1234@gmail.com>
2017-04-25 15:56:08 -0400

    Changed build-name to prevent network collisions with Ubisoft testing our game
    In reference to:
    https://www.facebook.com/groups/313348239045260/permalink/40165114021496
    9/
    
    Seems like they still mess with the games (huh)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -38,7 +38,7 @@ public class GameManager : MonoBehaviour {
 	//public static string ASSETSPATH = Application.dataPath;
 
 	// game build
-	private string BUILD = "final-build";
+	private string BUILD = "final-test-build";
 
 	// static instance of GameManager
 	public static GameManager instance = null;
